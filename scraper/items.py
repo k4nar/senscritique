cf919162c2b66de033c33e6c00ad4b39fb5c5f5f
Yannick PEROUX <yannick.peroux@gmail.com>
2014-03-19 21:06:08 +0000

    Add default values for items

--- scraper/items.py
+++ scraper/items.py
@@ -1,19 +1,26 @@
 from scrapy.item import Item, Field
 
-class UserItem(Item):
+class BaseItem(Item):
+    def __init__(self, *args, **kwargs):
+        super(Item, self).__init__(*args, **kwargs)
+
+        for field in self.fields.keys():
+            self.setdefault(field, None)
+
+class UserItem(BaseItem):
     uid = Field()
     uri = Field()
     age = Field()
     gender = Field()
     postcode = Field()
 
-class ProductItem(Item):
+class ProductItem(BaseItem):
     pid = Field()
     uri = Field()
     category = Field()
     name = Field()
 
-class RatingItem(Item):
+class RatingItem(BaseItem):
     uid = Field()
     pid = Field()
     score = Field()
