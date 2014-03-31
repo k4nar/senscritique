ea2988e5f1df6958e3bc91671b270dd7f512882e
Yannick PEROUX <yannick.peroux@gmail.com>
2014-03-31 03:11:49 +0100

    Add rule generation

--- miner/apriori.py
+++ miner/apriori.py
@@ -1,7 +1,7 @@
 import os
 import cPickle as pickle
+import json
 
-from multiprocessing import Pool
 from itertools import chain, count, combinations
 from collections import Counter
 
@@ -9,15 +9,16 @@ from .db import User, Rating, Product, init
 
 init()
 
-minsup = 0.03
+minsup = 0.33
+minconf = 0.8
 
 if os.path.exists('transactions'):
     with open('transactions') as f:
         transactions = pickle.load(f)
 else:
     transactions = filter(None, (
-        frozenset(t[0] for t in user.rating_set.select(Rating.pid).where(Rating.recommended).tuples())
-        for user in User.select().limit(1000)
+        frozenset(t[0] for t in user.rating_set.select(Rating.pid).join(Product).where(Rating.recommended and Product.category == 'film').tuples())
+        for user in User.select()
     ))
 
     with open('transactions', 'w+') as f:
@@ -27,38 +28,67 @@ if os.path.exists('associations'):
     with open('associations') as f:
         result = pickle.load(f)
 else:
-    support = Counter(chain(*transactions))
+    counter = Counter(chain(*transactions))
     minfreq = minsup * len(transactions)
     print minfreq
-    print support.most_common(1)
-    l1 = set(item for item, freq in support.items() if freq >= minfreq)
+    l1 = set(item for item, freq in counter.items() if freq >= minfreq)
     print 1, len(l1)
 
     print "before", len(transactions)
     transactions = filter(lambda t: len(t) >= 2, (t & l1 for t in transactions))
     print "after", len(transactions)
 
-    prev_l = set(frozenset((item,)) for item in l1)
-    result = {}
+    result = {frozenset((item,)): counter[item] for item in l1}
+    prev_l = result.keys()
 
     for k in count(2):
+        print "candidates"
         candidates = (frozenset(e) for e in (a | b for a in prev_l for b in prev_l) if len(e) == k)
         candidates = (c for c in set(candidates) if all(set(subset) in prev_l for subset in combinations(c, k - 1)))
+        print "done"
 
-        pool = Pool()
-        l = set(pool.map(lambda c: c if sum(c <= t for t in transactions) >= minfreq else None, candidates))
-        l.discard(None)
+        l = set()
+        for c in candidates:
+            freq = sum(1 for t in transactions if c <= t)
+            if freq >= minfreq:
+                l.add(c)
+                result[c] = freq
 
         print k, len(l)
 
         if not l:
             break
 
-        result[k] = l
         prev_l = l
 
     with open('associations', 'w+') as f:
         pickle.dump(result, f, pickle.HIGHEST_PROTOCOL)
 
-from pprint import pprint
-pprint(result)
+rules = list()
+for items, freq in result.items():
+    if len(items) < 2:
+        continue
+
+    candidates = (frozenset(r) for k in range(1, len(items)) for r in combinations(items, k))
+    confidant = set((items - r, r) for r in candidates if float(freq) / result[items - r] >= minconf)
+
+    if not confidant:
+        break
+
+    rules += confidant
+
+print len(rules)
+
+output = []
+nb_transactions = float(len(transactions))
+for l, r in rules:
+    conf = float(result[l | r]) / result[l]
+    lift = conf * (nb_transactions / result[r])
+    leverage = (result[l | r] / nb_transactions) - (result[l] / nb_transactions) * (result[r] / nb_transactions)
+
+    l, r = ([Product.get(Product.pid == pid).name for pid in items] for items in (l, r))
+
+    output.append({'l': l, 'r': r, 'conf': conf, 'lift': lift, 'leverage': leverage})
+
+with open("toto.json", 'w+') as f:
+    json.dump(output, f, indent=2)
