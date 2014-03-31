e7978c6e90ee0ba878acfd05c87ca850a2b3036a
Yannick PEROUX <yannick.peroux@gmail.com>
2014-03-31 13:34:35 +0100

    Move results in a separate directory

--- miner/mining.py
+++ miner/mining.py
@@ -5,35 +5,22 @@ import cPickle as pickle
 from .db import User, Rating, Product, init
 from .apriori import apriori
 
-
-
-# if os.path.exists('transactions'):
-#     with open('transactions') as f:
-#         transactions = pickle.load(f)
-# else:
-#     transactions = filter(None, (
-#         frozenset(t[0] for t in user.rating_set.select(Rating.pid).join(Product).where(Rating.recommended and Product.category == 'film').tuples())
-#         for user in User.select()
-#     ))
-
-#     with open('transactions', 'w+') as f:
-#         pickle.dump(transactions, f, pickle.HIGHEST_PROTOCOL)
-
 minsup = 0.2
 minconf = 0.75
 
-classes = {
-    "all": User.select(),
-    "men": User.select().where(User.gender == 'm'),
-    "men_20-30": User.select().where(User.gender == 'm' and User.age.between(20, 30)),
-    "men_30-80": User.select().where(User.gender == 'm' and User.age.between(30, 80)),
-    "women": User.select().where(User.gender == 'f'),
-    "women_20-30": User.select().where(User.gender == 'f' and User.age.between(20, 30)),
-    "women_30-80": User.select().where(User.gender == 'f' and User.age.between(30, 80)),
+panels = {
+    "all": None,
+    "men": User.gender == 'm',
+    "men_20-30": User.gender == 'm' and User.age.between(20, 30),
+    "men_30-80": User.gender == 'm' and User.age.between(30, 80),
+    "women": User.gender == 'f',
+    "women_20-30": User.gender == 'f' and User.age.between(20, 30),
+    "women_30-80": User.gender == 'f' and User.age.between(30, 80),
 }
 
+
 def get_targets():
-    targets = {
+    t = {
         "recommended": Rating.recommended,
         "liked": Rating.score > 8,
         "disliked": Rating.score < 3
@@ -41,20 +28,20 @@ def get_targets():
 
     for category in ['film', 'jeuvideo', 'serie']:
         for name in ['recommended', 'liked', 'disliked']:
-            targets[name + "_" + category] = targets[name] and Product.category == category
+            t[name + "_" + category] = t[name] and Product.category == category
 
-    return targets
+    return t
 
 init()
-for class_name, users in classes.items():
-    users = users.execute()
+for panel_name, panel in panels.items():
+    users = User.select().where(panel)
 
     for target, criteria in get_targets().items():
-        name = class_name + "_" + target
+        name = panel_name + "_" + target
 
         print name
 
-        trans_file = name + ".trans"
+        trans_file = "results/" + name + ".trans"
         if os.path.exists(trans_file):
             with open(trans_file) as f:
                 transactions = pickle.load(f)
@@ -67,7 +54,7 @@ for class_name, users in classes.items():
             with open(trans_file, 'w+') as f:
                 pickle.dump(transactions, f, pickle.HIGHEST_PROTOCOL)
 
-        rules = apriori(transactions, minsup, minconf, name + ".freq")
+        rules = apriori(transactions, minsup, minconf, "results/" + name + ".freq")
 
         output = []
         for rule, (conf, lift, leverage) in rules.items():
