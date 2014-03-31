7af60b21c494f867638c1d10ce1eb75903892fbb
Yannick PEROUX <yannick.peroux@gmail.com>
2014-03-31 04:42:10 +0100

    Persist transactions

--- miner/mining.py
+++ miner/mining.py
@@ -54,10 +54,18 @@ for class_name, users in classes.items():
 
         print name
 
-        transactions = filter(None, (
-            set(t[0] for t in user.rating_set.select(Rating.pid).join(Product).where(criteria).tuples())
-            for user in users
-        ))
+        trans_file = name + ".trans"
+        if os.path.exists(trans_file):
+            with open(trans_file) as f:
+                transactions = pickle.load(f)
+        else:
+            transactions = filter(None, (
+                set(t[0] for t in user.rating_set.select(Rating.pid).join(Product).where(criteria).tuples())
+                for user in users
+            ))
+
+            with open(trans_file, 'w+') as f:
+                pickle.dump(transactions, f, pickle.HIGHEST_PROTOCOL)
 
         rules = apriori(transactions, minsup, minconf, name + ".freq")
 
