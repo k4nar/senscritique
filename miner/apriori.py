9675b8fb2c18a21ced3bf53f0dbf42a04d96d1a5
Yannick PEROUX <yannick.peroux@gmail.com>
2014-04-01 22:04:26 +0100

    Add comments to mining.py

--- miner/apriori.py
+++ miner/apriori.py
@@ -7,56 +7,92 @@ from collections import Counter
 
 
 def apriori(transactions, minsup, minconf, freq_file=None):
+    """
+    Run the Apriori algorithm with the given transactions.
+    The transactions must be a list of sets of items.
+
+    Returns the rules as a dictionary.
+    """
+
+    # Compute the number of transactions in order to calculate the support
     nb_transactions = float(len(transactions))
 
+    # We get the frequent itemsets from if they were persisted
     if freq_file and os.path.exists(freq_file):
         with open(freq_file) as f:
             frequencies = pickle.load(f)
     else:
+        # Count each item in the transactions
         counter = Counter(chain(*transactions))
+        # Get the supported items
         l1 = set(item for item, freq in counter.items() if freq / nb_transactions >= minsup)
 
+        # We delete all the items in the transactions that are not supported,
+        # as they are now useless for the following calculations
         transactions = [t & l1 for t in transactions]
 
+        # We start constructing the dictionary containing the frequent itemsets
+        # with the first iteration
         frequencies = {frozenset((item,)): counter[item] for item in l1}
+
+        # We store the previous itemsets at each iteration in order to compute
+        # the candidates
         prev_l = frequencies.keys()
 
         for k in count(2):
+            # We filter the transactions of cardinality lesser than k
             transactions = filter(lambda t: len(t) >= k, transactions)
 
+            # We compute the candidates using the previous itemsets.
+            # This is a generator, each value will be calculated when needed
             candidates = (frozenset(e) for e in (a | b for a in prev_l for b in prev_l) if len(e) == k)
-            candidates = (c for c in set(candidates) if all(set(subset) in prev_l for subset in combinations(c, k - 1)))
+            candidates = (
+                c for c in set(candidates) if all(set(subset) in prev_l
+                for subset in combinations(c, k - 1))
+            )
 
+            # We filter the candidates in order to get the supported itemsets
             l = set()
             for c in candidates:
+                # frequency of the candidate
                 freq = sum(1 for t in transactions if c <= t)
+
                 if freq / nb_transactions >= minsup:
                     l.add(c)
                     frequencies[c] = freq
 
+            # If we didn't found any supported itemsets, we stop
             if not l:
                 break
 
             prev_l = l
 
+        # We persist the frequent itemsets in a file if provided
         if freq_file:
             with open(freq_file, 'w+') as f:
                 pickle.dump(frequencies, f, pickle.HIGHEST_PROTOCOL)
 
+    # We can now generate the rules
     rules = {}
     for items, freq in frequencies.items():
         if len(items) < 2:
             continue
 
+        # the support of the current itemset
         support = frequencies[items] / nb_transactions
-        candidates = set(frozenset(r) for k in range(1, len(items)) for r in combinations(items, k))
 
+        # We compute all the possible candidates
+        candidates = (frozenset(r) for k in range(1, len(items)) for r in combinations(items, k))
+
+        # We filter the candidates using minconf
         for r in candidates:
             l = items - r
             conf = float(freq) / frequencies[l]
+
             if conf >= minconf:
                 lift = conf * (nb_transactions / frequencies[r])
                 leverage = support - (frequencies[l] * frequencies[r]) / (nb_transactions ** 2)
+
                 rules[(l, r)] = (conf, lift, leverage)
 
     return rules
--- miner/mining.py
+++ miner/mining.py
@@ -8,6 +8,7 @@ from .apriori import apriori
 minsup = 0.1
 minconf = 0.8
 
+# The panels of users we want to use
 panels = {
     "all": None,
     "men": User.gender == 'm',
@@ -16,23 +17,28 @@ panels = {
     "youngest": User.age <= 25,
 }
 
+# The different criterias used to split the ratings
 criterias = {
     'recommended': Rating.recommended,
     'disliked': Rating.score < 3
 }
 
+# The categories used to split the products
+categories = ['film', 'jeuvideo']
 
-def generate(e):
+
+def generate(panel):
     init()
 
-    panel_name, panel = e
+    panel_name, panel = panel
 
-    for category in ['film', 'jeuvideo']:
-        for critera_name, critera in criterias:
-            name = panel_name + "_" + category + "_" + critera_nam
+    for category in categories:
+        for critera_name, criteria in criterias.items():
+            name = panel_name + "_" + category + "_" + critera_name
 
-            print name
-
+            # We get the transactions
+            # the transactions are persisted in order to be able to launch
+            # the algorithm a second time without hitting the database
             trans_file = "results/" + name + ".trans"
             if os.path.exists(trans_file):
                 with open(trans_file) as f:
@@ -53,21 +59,27 @@ def generate(e):
                 with open(trans_file, 'w+') as f:
                     pickle.dump(transactions, f, pickle.HIGHEST_PROTOCOL)
 
+            # We get the rules from the apriori function
             rules = apriori(transactions, minsup, minconf, "results/" + name + ".freq")
 
-            rules_export = []
-            for rule, (conf, lift, leverage) in rules.items():
-                l, r = ([Product.get(Product.pid == pid).name for pid in items] for items in rule)
-                rules_export.append({'l': l, 'r': r, 'conf': conf, 'lift': lift, 'leverage': leverage})
-
+            # We export the rules in json
             with open("results/" + name + ".json", 'w+') as f:
+                rules_export = []
+                for rule, (conf, lift, leverage) in rules.items():
+                    l, r = ([Product.get(Product.pid == pid).name for pid in items] for items in rule)
+                    rules_export.append({'l': l, 'r': r, 'conf': conf, 'lift': lift, 'leverage': leverage})
+
                 output = {
                     'minconf': minconf,
                     'minsup': minsup,
                     'rules': rules_export
                 }
+
                 json.dump(output, f, indent=2)
 
-from multiprocessing import Pool
-pool = Pool()
-pool.map(generate, panels.items())
+
+if __name__ == '__main__':
+    # We launch the generation in separate processes
+    from multiprocessing import Pool
+    pool = Pool()
+    pool.map(generate, panels.items())
